ff73db33d0113c4294f81e73155f31fd770289d2
mengshalitsofi <mengshalitsofi@gmail.com>
2021-02-28 12:39:31 -0800

    commit tree 9f8f4c9f009e791d9e2b19b7e6985068e7c0de1d

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do   
+      Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
+      Ship.new(params[:pirate][:ships][1][:name], params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
+
+      erb :"pirates/show"
+    end
 
   end
 end
